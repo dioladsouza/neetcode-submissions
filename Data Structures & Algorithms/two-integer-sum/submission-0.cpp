bacb87c4b52f5631ab0d18957dcdb0c25fdcacb5
Diola Chryselle Dsouza <diocrisel9@gmail.com>
2026-08-21 14:12:13 -0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mpp;
+        int a, b;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            a = nums[i];
+            b = target - a;
+            if(mpp.find(b) != mpp.end())
+                return {mpp[b], i};
+            mpp[a] = i;
+        }
+        return {-1, -1};
+    }
+};
