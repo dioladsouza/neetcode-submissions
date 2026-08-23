808be5bd55684f8e59df5afc935f6b1ac45a57e9
Diola Chryselle Dsouza <diocrisel9@gmail.com>
2026-08-23 16:24:07 -0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool box[9][9] = {false};
+
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                if(board[i][j] == '.')
+                    continue;
+                int num = board[i][j] - '1';
+                int box_no = (i / 3) * 3 + (j / 3);
+
+                if(rows[i][num] || cols[j][num] || box[box_no][num])
+                    return false;
+                rows[i][num] = true;
+                cols[j][num] = true;
+                box[box_no][num] = true;
+            }
+        }
+        return true;
+    }
+};
